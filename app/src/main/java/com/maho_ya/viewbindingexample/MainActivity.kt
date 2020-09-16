db6065ef1e9ac29c413f7bcf0e674cc81c363e65
mahoya <support@maho-ya.com>
2020-09-16 20:25:41 +0900

    Add simple ViewBinding example on Activity

--- app/src/main/java/com/maho_ya/viewbindingexample/MainActivity.kt
+++ app/src/main/java/com/maho_ya/viewbindingexample/MainActivity.kt
@@ -2,10 +2,34 @@ package com.maho_ya.viewbindingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.maho_ya.viewbindingexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // View Binding
+    // https://developer.android.com/topic/libraries/view-binding?hl=ja
+
+    // layout/**.xml 毎にバインディングクラスが xmlのファイル名 + Binding の名称でキャメルケースで生成される。
+    // 生成されたバインディングクラスにはrootとidを持つViewへの参照が組み込まれる。
+    // idを持たないものは参照が生成されない。
+    // またレイアウトのルート要素に tools:viewBindingIgnore="true" が設定された場合は Bindingクラスは生成されない。
+
+
+    // 生成されたBindingクラスを使用する。
+    // クラスが実際に生成されるのはBuild時だが、Buildしなくてもエラーにはならない。
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // View Bindingのセットアップ方法
+        // - Bindingクラスのインスタンスを生成
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        // - rootのViewをsetContentViewに渡して、アクティブビューにする
+        setContentView(view)
+
+        // 以上で任意のビューを参照できるようになる
+        binding.textView.text = "BindingTest"
     }
 }
